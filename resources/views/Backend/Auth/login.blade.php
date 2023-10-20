0c73a000f237c457dfad39a7edf212cdd8f1ba48
AnhBH <anh.bh@hostingviet.vn>
2023-10-20 17:37:02 +0700

    AnhBH - complete theme login

--- resources/views/Backend/Auth/login.blade.php
+++ resources/views/Backend/Auth/login.blade.php
@@ -2,43 +2,50 @@
 @section('title', 'Login to Admin Panel')
 
 @section('body')
-    <div class='relative w-screen h-screen flex justify-center items-center '>
-        <div class="absolute top-0 left-0 right-0 w-full h-full p-5 ">
-            <div class=" bg-gradient-to-r from-sky-300 to-indigo-200 w-full h-full drop-shadow-xl rounded-xl blur-sm">
+    <div class='w-screen h-screen flex justify-center items-center items-center px-10 py-5'>
+        <div class="w-1/2 flex flex-col gap-3 p-60">
+            <h1 class="text-5xl font-bold mb-2 text-sky-500">Welcome back</h1>
+            <h2 class="text-md font-normal mb-5 text-gray-500">Enter your email and password to sign in</h2>
+            <div>
+                <form action="POST" class="">
+                    @csrf
+                    @method('POST')
+                    <div class="flex flex-col gap-5 justify-items-start items-start">
+                        <div class="block w-full">
+                            <x-input.form-input
+                                id="email_login"
+                                type="email"
+                                label="Email"
+                                name="user_email"
+                                icon='<i class="bi bi-envelope"></i>'
+                                required="true"/>
+                        </div>
+                        <div class="block w-full">
+                            <x-input.form-input
+                                id="password_login"
+                                type="password"
+                                label="Password"
+                                name="user_email"
+                                icon='<i class="bi bi-key"></i>'
+                                required="true"/>
+                        </div>
+                    </div>
+                    <label class="relative inline-flex items-center my-4 cursor-pointer">
+                        <input type="checkbox" value="" class="sr-only peer">
+                        <div class="w-9 h-5 bg-gray-100 rounded-full peer peer-focus:ring-3 peer-focus:ring-sky-300 dark:peer-focus:ring-sky-300 dark:bg-slate-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute ease-in-out duration-300 after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-sky-400"></div>
+                        <span class="ml-3 text-sm font-medium text-gray-900">Remember login</span>
+                    </label>
+                    <div class="flex justify-between mt-3">
+                        <button type="submit" class="text-white bg-gradient-to-br from-sky-500 to-blue-300 transition-all duration-200 focus:ring-4 hover:shadow-xl focus:ring-sky-200  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 w-full">
+                            Sign in
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
-        <div class="xl:w-1/4 md:w-1/2 h-auto bg-white rounded-lg drop-shadow-xl p-8">
-            <h1 class="text-3xl font-bold mb-2">Welcome back</h1>
-            <h2 class="text-md font-normal mb-5">Enter your email and password to sign in</h2>
-            <form action="POST" class="">
-                @csrf
-                @method('POST')
-                <div class="flex flex-col gap-5 justify-items-start items-start w-full">
-                    <div class="block w-full">
-                        <x-input.form-input
-                            id="email_login"
-                            type="email"
-                            label="Email"
-                            name="user_email"
-                            icon='<i class="bi bi-envelope"></i>'
-                            required="true"/>
-                    </div>
-                    <div class="block w-full">
-                        <x-input.form-input
-                            id="password_login"
-                            type="password"
-                            label="Password"
-                            name="user_email"
-                            icon='<i class="bi bi-key"></i>'
-                            required="true"/>
-                    </div>
-                </div>
-                <div class="flex justify-between mt-3">
-                    <button type="submit" class="border-2 py-2 px-6 rounded-2xl bg-sky-200 font-bold ">
-                        Sign in
-                    </button>
-                </div>
-            </form>
+        <div class="w-1/2 h-full p-0 relative overflow-hidden rounded-xl">
+            <div class="absolute -right-1/4 bottom-4 rotate-12 bg-gradient-to-b from-sky-500 to-lime-300 w-full h-screen rounded-xl">
+            </div>
         </div>
     </div>
 @endsection
