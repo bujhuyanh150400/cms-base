d52e1a42c5ba4813b89222b6f502bd282f7ddfb8
AnhBH <anh.bh@hostingviet.vn>
2023-11-06 17:20:18 +0700

    AnhBH - dap di xay lai bang alpinejs

--- resources/views/Backend/Auth/login.blade.php
+++ resources/views/Backend/Auth/login.blade.php
@@ -6,60 +6,59 @@
     <title>{{$title ?? 'LARAVEL'}}</title>
     @vite(['resources/css/app.css', 'resources/scss/app.scss', 'resources/js/app.js'])
 </head>
-<body class="fixed w-screen h-screen flex justify-between items-center p-5">
-<div
-    class="h-auto flex flex-col justify-center gap-5 p-5 bg-white z-10 lg:w-1/2 lg:basis-1/2 w-full rounded-3xl shadow-2xl lg:shadow-none lg:rounded-none">
-    <h1 class="text-5xl font-bold mb-2 text-sky-500">Welcome back</h1>
-    <h2 class="text-md font-normal mb-5 text-gray-500">Hãy điền email và password để đăng nhập</h2>
-    @error('login')
-    <div class="p-4 my-2 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
-        <span class="font-medium">{{ $message }}</span>
-    </div>
-    @enderror
-    <form method="POST" action="{{route('login-admin')}}">
-        @csrf
-        @method('POST')
-        <div class="flex flex-col gap-5 justify-items-start items-start">
-            <div class="block w-full">
-                @error('email')
-                <label class="text-sm font-medium text-red-400 mb-2">
-                    <strong>{{ $message }}</strong>
-                </label>
-                @enderror
-                <x-input.form-input id="email_login" type="text" label="Email" name="email" value="{{old('email')}}"
-                                    icon='<i class="bi bi-envelope"></i>' required="false"/>
-            </div>
-            <div class="block w-full">
-                @error('password')
-                <label class="text-sm font-medium text-red-400 mb-2">
-                    <strong>{{ $message }}</strong>
-                </label>
-                @enderror
-                <x-input.form-input id="password_login" type="password" label="Password" value="{{old('password')}}"
-                                    name="password" icon='<i class="bi bi-key"></i>' required="false"/>
-            </div>
+<body>
+<main class="w-screen h-screen grid grid-cols-2">
+    <div class="h-auto flex flex-col justify-center gap-5 p-5 bg-white z-10 w-full">
+        <h1 class="text-5xl font-bold mb-2 text-sky-500">Welcome back</h1>
+        <h2 class="text-md font-normal mb-5 text-gray-500">Hãy điền email và password để đăng nhập</h2>
+        @error('login')
+        <div class="p-4 my-2 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
+            <span class="font-medium">{{ $message }}</span>
         </div>
-        <label class="relative inline-flex items-center my-4 cursor-pointer">
-            <input type="checkbox" name="remember_me" value="1" class="sr-only peer" {{ old('remember_me') ? 'checked' : '' }}>
-            <div
-                class="w-9 h-5 bg-gray-100 rounded-full peer peer-focus:ring-3 peer-focus:ring-sky-300 dark:peer-focus:ring-sky-300 dark:bg-slate-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute ease-in-out duration-300 after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-sky-400">
+        @enderror
+        <form method="POST" action="{{route('login-admin')}}">
+            @csrf
+            @method('POST')
+            <div class="flex flex-col gap-5 justify-items-start items-start">
+                <div class="block w-full">
+                    @error('email')
+                    <label class="text-sm font-medium text-red-400 mb-2">
+                        <strong>{{ $message }}</strong>
+                    </label>
+                    @enderror
+                    <x-input.form-input id="email_login" type="text" label="Email" name="email" value="{{old('email')}}"
+                                        icon='<i class="bi bi-envelope"></i>' required="false"/>
+                </div>
+                <div class="block w-full">
+                    @error('password')
+                    <label class="text-sm font-medium text-red-400 mb-2">
+                        <strong>{{ $message }}</strong>
+                    </label>
+                    @enderror
+                    <x-input.form-input id="password_login" type="password" label="Password" value="{{old('password')}}"
+                                        name="password" icon='<i class="bi bi-key"></i>' required="false"/>
+                </div>
             </div>
-            <span class="ml-3 text-sm font-medium text-gray-900">Remember login</span>
-        </label>
-        <div class="flex justify-between mt-3">
-            <button type="submit"
-                    class="text-white bg-gradient-to-br from-sky-500 to-blue-300 transition-all duration-200 focus:ring-4 hover:shadow-xl focus:ring-sky-200  font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full">
-                Sign in
-            </button>
-        </div>
-    </form>
-</div>
-<div class="lg:w-1/2 lg:basis-1/2 lg:block md:hidden h-full p-0 relative overflow-hidden rounded-xl">
-    <div
-        class="absolute -right-1/4 bottom-10 rotate-12 bg-gradient-to-b from-sky-500 to-lime-300 w-full h-full rounded-xl">
+            <label class="relative inline-flex items-center my-4 cursor-pointer">
+                <input type="checkbox" name="remember_me" value="1"
+                       class="sr-only peer" {{ old('remember_me') ? 'checked' : '' }}>
+                <div
+                    class="w-9 h-5 bg-gray-100 rounded-full peer peer-focus:ring-3 peer-focus:ring-sky-300 dark:peer-focus:ring-sky-300 dark:bg-slate-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute ease-in-out duration-300 after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-sky-400">
+                </div>
+                <span class="ml-3 text-sm font-medium text-gray-900">Remember login</span>
+            </label>
+            <div class="flex justify-between mt-3">
+                <button type="submit"
+                        class="text-white bg-gradient-to-br from-sky-500 to-blue-300 transition-all duration-200 focus:ring-4 hover:shadow-xl focus:ring-sky-200  font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full">
+                    Sign in
+                </button>
+            </div>
+        </form>
     </div>
-</div>
-<div class="lg:hidden md:fixed absolute inset-0 bg-gradient-to-b from-sky-500 to-lime-300 w-full h-full z-0">
-</div>
+    <div class="w-full h-full p-10 ">
+        <div class="bg-gradient-to-r from-cyan-500 to-blue-500 w-full h-full rounded-2xl shadow-2xl">
+        </div>
+    </div>
+</main>
 </body>
 </html>
--- resources/views/Layouts/layout_backend.blade.php
+++ resources/views/Layouts/layout_backend.blade.php
@@ -1,185 +1,28 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
     @vite(['resources/css/app.css', 'resources/scss/app.scss', 'resources/js/app.js'])
 </head>
+
 <body class="p-0 m-0 bg-stone-200">
-    <div class="absolute min-h-[300px] bg-orange-500 w-full top-0 left-0 z-[-1]"></div>
-
-    <div id="toast-section">
-    </div>
-
-    <x-container.navbar-admin/>
-    <main class="section-main-admin">
-        <x-container.header-admin>
-            <x-slot name="headerTitle">@yield('title-header')</x-slot>
-        </x-container.header-admin>
+    <main class="flex ">
+        {{--        <form action="{{ route('logout-admin') }}" method="POST"> --}}
+        {{--            @csrf --}}
+        {{--            @method('POST') --}}
+        {{--            <button type="submit" --}}
+        {{--                    class="block w-full text-left px-4 py-2 transition text-red-400 duration-200 hover:bg-gray-400 hover:text-white" --}}
+        {{--            > --}}
+        {{--                <i class="bi bi-box-arrow-right"></i> Log out --}}
+        {{--            </button> --}}
+        {{--        </form> --}}
         <div class="py-5">
             @yield('main')
         </div>
     </main>
 </body>
+
 </html>
-
-<script type="module">
-    $(document).on('click', function(e) {
-        let target = $(e.target);
-        if (!target.closest('.drop_down_container').length) {
-            $('.drop_down_list').slideUp(100);
-        }
-    });
-    $('.drop_drown_btn').on('click', function() {
-        let dropdown = $('#' + $(this).data('dropdown'));
-        $('.drop_down_list').not(dropdown).hide();
-        dropdown.slideToggle(100);
-    });
-    $('.menu-list-btn').on('click',function (){
-        const menuList = $(this).parent();
-        menuList.find('.sub-menu-list').slideToggle(100);
-    })
-    $(document).ready(function (){
-        $('.menu-list').each(function (index,item){
-            if($(this).find('.active-menu-sub').length == 1){
-                $(this).find('.sub-menu-list').show();
-                $(this).find('.menu-list-btn').addClass('active-menu');
-            }
-        })
-    });
-    $('#admin-btn_show-hide').on('click',function (){
-        if($('.navbar-admin').hasClass('hidden-navbar-admin') == true){
-            $(this).html('<i class="bi bi-list-nested"></i>');
-            $('.navbar-admin').removeClass('hidden-navbar-admin');
-            $('.section-main-admin').removeClass('section-main-admin-hidden-navbar')
-        }else{
-            $(this).html('<i class="bi bi-list"></i>');
-            $('.navbar-admin').addClass('hidden-navbar-admin');
-            $('.section-main-admin').addClass('section-main-admin-hidden-navbar')
-
-        }
-    });
-
-
-    const app = {
-        select2Custom: (element) => {
-            element.each(function (index,item){
-                const select = $(item);
-                select.hide();
-                select.wrap('<div class="select-custom-container"></div>');
-                select.before('<button type="button" class="select-custom-button"></button>');
-                select.after('<div class="select-custom-dropdown"></div>');
-                const dropdown = select.next('.select-custom-dropdown');
-                const option = select.find('option');
-                dropdown.append('<input type="text" class="select-custom-search" placeholder="Search...">');
-                dropdown.append('<ul class="select-custom-results"></ul>');
-                const searchInput = dropdown.find('.select-custom-search');
-                const resultsList = dropdown.find('.select-custom-results');
-                const buttonSelect = select.parent('.select-custom-container').find('.select-custom-button');
-                buttonSelect.on('click',function (){
-                    dropdown.slideToggle(100);
-                    $('.select-custom-dropdown').not(select.parent('.select-custom-container').find('.select-custom-dropdown')).hide();
-                });
-                option.each(function() {
-                    const value = $(this).val();
-                    const text = $(this).text();
-                    if($(this).prop('selected')){
-                        buttonSelect.text(text);
-                    }else{
-                        buttonSelect.text(option.first().text());
-                    }
-                    resultsList.append(`<li data-value="${value}">${text}</li>`);
-                });
-                searchInput.on('input', function() {
-                    let searchTerm = $(this).val().toLowerCase();
-                    resultsList.find('li').each(function() {
-                        let text = $(this).text().toLowerCase();
-                        if (text.includes(searchTerm)) {
-                            $(this).show();
-                        } else {
-                            $(this).hide();
-                        }
-                    });
-                });
-                resultsList.on('click', 'li', function() {
-                    let value = $(this).data('value');
-                    let text = $(this).text();
-                    buttonSelect.text(text);
-                    select.val(value);
-                    select.trigger('change');
-                    dropdown.slideUp(100);
-                    searchInput.val('');
-                    resultsList.find('li').show();
-                });
-                $(document).on('click', function(e) {
-                    let target = $(e.target);
-                    if (!target.closest('.select-custom-container').length) {
-                        $('.select-custom-dropdown').slideUp(100);
-                    }
-                });
-            })
-        },
-
-
-        showToast: (message,type = 'success',timeout = 5000) =>{
-            let icon;
-            switch (type){
-                case 'error':
-                    icon = '<i class="bi bi-exclamation-lg"></i>';
-                    break;
-                case 'warning':
-                    break;
-                    icon = '<i class="bi bi-cone"></i>';
-                case 'success':
-                    icon = '<i class="bi bi-check"></i>';
-                    break;
-                default:
-                    type = 'warning';
-                    icon = '<i class="bi bi-cone"></i>';
-                    break;
-            }
-            const container = $('#toast-section');
-            let html =`
-                <div class="toast-container" role="${type}">
-                    <div class="icon-holder">
-                           ${icon}
-                    </div>
-                    <div class="content">${message}</div>
-                    <button type="button" class="btn-close">
-                        <i class="bi bi-x"></i>
-                    </button>
-                </div>
-            `;
-            container.append(html);
-            $('.toast-container').each(function (index,item){
-                $(item).on('click',function (){
-                    $(this).hide(100);
-                    setTimeout(()=>{
-                        $(this).remove()
-                    },100)
-                })
-                setTimeout(()=>{
-                    $(this).fadeOut(3000);
-                    setTimeout(()=>{
-                        $(this).remove()
-                    },3000)
-                },timeout)
-            })
-
-        }
-    }
-    $(document).ready(function() {
-        app.select2Custom($('.customSelect'));
-    });
-    @if(session('success'))
-        app.showToast('{{ session('success') }}','success');
-    @endif
-    @if(session('error'))
-        app.showToast('{{ session('error') }}','error');
-    @endif
-</script>
-
-<script type="module">
-
-</script>
